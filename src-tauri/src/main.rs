21e807933133fa6de0302c20bf4a9d01e60480b1
Cassie <348040933@qq.com>
2022-06-21 17:16:08 +0800

    添加打包配置

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,24 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{GlobalWindowEvent, WindowEvent, Wry};
+
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
+      .on_window_event(|event|{
+        match event.event() {
+            WindowEvent::Resized(_) => {}
+            WindowEvent::Moved(_) => {}
+            WindowEvent::CloseRequested { .. } => {}
+            WindowEvent::Destroyed => {}
+            WindowEvent::Focused(_) => {}
+            WindowEvent::ScaleFactorChanged { .. } => {}
+            WindowEvent::FileDrop(_) => {}
+            WindowEvent::ThemeChanged(_) => {}
+            _=>{}
+        }
+      })
     .run(context)
     .expect("error while running tauri application");
 }
